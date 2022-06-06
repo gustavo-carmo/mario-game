a634e18945c832d6d62ba4c207138d2e3226848f
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2022-06-06 23:58:11 +0100

    add the jump function

--- js/index.js
+++ js/index.js
@@ -4,15 +4,29 @@ const pipe = document.getElementById("pipe");
 const keysListener = [32, 38, 87];
 
 const jump = (e) => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+  /*
   var event = window.event ? window.event : e;
 
   if (keysListener.includes(event.keyCode)) {
-    mario.classList.add("jump");
-
-    setTimeout(() => {
-      mario.classList.remove("jump");
-    }, 500);
-  }
+    
+  }*/
 };
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = Number(
+    window.getComputedStyle(mario).bottom.replace("px", "")
+  );
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+  }
+}, 10);
 
 document.addEventListener("keyup", jump);
